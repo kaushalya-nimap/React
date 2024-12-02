abacaf2045a3736e0b1ce1c216870ba4ca09fc56
kaushalya-nimap <kaushalya.p@nimapinfotech.com>
2024-12-02 15:47:47 +0530

    Error boundary usage

---
+++ ErrorBoundary/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from './Hero'
+import ErrorBoundary from './ErrorBoundary'
+
+function App() {
+  return (
+  <div>
+     <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
+  </div>    
+  )
+}
+
+export default App
